a581e38257ddecc3e92537ab0eb9e39f9df1b64d
Thomas Taylor <thomas.taylor@kineo.com>
2017-01-16 23:16:58 +0000

    Remove unused imports

--- index.php
+++ index.php
@@ -8,12 +8,8 @@
 
 		<title>Code | taylortom.co.uk</title>
 
-        <link href='http://fonts.googleapis.com/css?family=Oxygen:400,700,300|Source+Code+Pro:400,700' rel='stylesheet' type='text/css'>
-
-        <link rel="stylesheet" href="theme.css" />
-
-		<script type="text/javascript" src="js/libs/jquery-1.10.2.min.js"></script>
-		<script type="text/javascript" src="js/main.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Oxygen:400,700,300|Source+Code+Pro:400,700' rel='stylesheet' type='text/css'>
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc=" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<style>
